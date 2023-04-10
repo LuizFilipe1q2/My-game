bd9c4099246decfefb178d28d5277a644b3113f4
LuizFilipe1q2 <114111639+LuizFilipe1q2@users.noreply.github.com>
2023-04-10 16:56:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,11 +17,11 @@ var score = 0
 
 
 function setup() {
-  createCanvas(600, 1800);
-  canvas.style = 'position:absolute;top:0px;right:40%;button:0px;margin:auto;border:2px solid black';
-  bola = createSprite(random(400, 221), random(400, 400), 12, 12);
-  bola2 = createSprite(random(10, 339), random(10, 339), 12, 12);
-  Jogador = createSprite(200, 400, 100, 15);
+  createCanvas(1000, 1000);
+  canvas.style = 'position:absolute;top:0px;right:20%;button:0px;margin:auto;border:2px solid black';
+  bola = createSprite(random(200, 800),600, 12, 12);
+ // bola2 = createSprite(random(10, 339), random(10, 339), 12, 12);
+  Jogador = createSprite(200, 700, 100, 15);
   Alvo = createSprite(230, 100, 15, 15);
   Alvo2 = createSprite(230, 120, 15, 15);
   Alvo3 = createSprite(2230, 140, 15, 15);
@@ -32,12 +32,18 @@ function setup() {
   Alvo8 = createSprite(250, 140, 15, 15);
   Alvo9 = createSprite(250, 160, 15, 15);
   Alvo10 = createSprite(250, 180, 15, 15);
-  Barreira = createSprite(500, 220, 290, 10);
-  Barreira2 = createSprite(20, 220, 290, 10);
-  bola.velocityX = -6;
-  bola.velocityY = -5;
-  bola2.velocityX = -6;
-  bola2.velocityY = -5;
+  Barreira = createSprite(1000, 500, 600, 10);
+  Barreira2 = createSprite(135, 500, 320, 10);
+  //bola2.velocityX = -6;
+ // bola2.velocityY = -5;
+  for (var Alvo11=10;Alvo11<1000;Alvo11=Alvo11+20) {
+
+  var Alvo12 = createSprite(Alvo11, 200, 15, 15);
+
+  Alvo12.shapeColor = 'green'
+
+}
+
 }
 
 function draw() {
@@ -45,9 +51,14 @@ function draw() {
   fill('white')
   textSize(30)
   text('score:  ' + score, 240, 800)
+  if (keyDown('space')) {
 
+    bola.velocityX = 8;
+    bola.velocityY = 8;
+
+  }
   bola.shapeColor = 'white';
-  bola2.shapeColor = 'white';
+  //  bola2.shapeColor = 'white';
   Jogador.shapeColor = 'white';
   Alvo.shapeColor = 'green';
   Alvo2.shapeColor = 'green';
@@ -61,80 +72,80 @@ function draw() {
   Alvo10.shapeColor = 'green';
   
   edges = createEdgeSprites();
-  bola.bounceOff(bola2);
+  //bola.bounceOff(bola2);
   bola.bounceOff(edges);
-  bola2.bounceOff(edges);
-  bola2.bounceOff(bola);
+ // bola2.bounceOff(edges);
+  //bola2.bounceOff(bola);
   Jogador.x = World.mouseX;
   bola.bounceOff(Jogador);
-  bola2.bounceOff(Jogador);
+  //bola2.bounceOff(Jogador);
   Jogador.bounceOff(edges);
   bola.bounceOff(Barreira);
-  bola2.bounceOff(Barreira);
+  //bola2.bounceOff(Barreira);
   bola.bounceOff(Barreira2);
-  bola2.bounceOff(Barreira2);
+ // bola2.bounceOff(Barreira2);
   
-  if (bola.isTouching(Alvo) || bola2.isTouching(Alvo)) {
+ 
+ if (bola.isTouching(Alvo) /*bola2.isTouching(Alvo))*/) {
     score = score + 1
     Alvo.destroy();
     bola.bounceOff(Alvo);
-    bola2.bounceOff(Alvo);
+   // bola2.bounceOff(Alvo);
   }
-  if (bola.isTouching(Alvo2) || bola2.isTouching(Alvo2)) {
+  if (bola.isTouching(Alvo2) /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo2.destroy();
     bola.bounceOff(Alvo2);
-    bola2.bounceOff(Alvo2);
+   // bola2.bounceOff(Alvo2);
   }
-  if (bola.isTouching(Alvo3) || bola2.isTouching(Alvo3)) {
+  if (bola.isTouching(Alvo3)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo3.destroy();
     bola.bounceOff(Alvo3);
-    bola2.bounceOff(Alvo3);
+   // bola2.bounceOff(Alvo3);
   }
-  if (bola.isTouching(Alvo4) || bola2.isTouching(Alvo4)) {
+  if (bola.isTouching(Alvo4)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo4.destroy();
     bola.bounceOff(Alvo4);
-    bola2.bounceOff(Alvo4);
+   // bola2.bounceOff(Alvo4);
   }
-  if (bola.isTouching(Alvo5) || bola2.isTouching(Alvo5)) {
+  if (bola.isTouching(Alvo5)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo5.destroy();
     bola.bounceOff(Alvo5);
-    bola2.bounceOff(Alvo5);
+    //bola2.bounceOff(Alvo5);
   }
-  if (bola.isTouching(Alvo6) || bola2.isTouching(Alvo6)) {
+  if (bola.isTouching(Alvo6)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo6.destroy();
     bola.bounceOff(Alvo6);
-    bola2.bounceOff(Alvo6);
+    //bola2.bounceOff(Alvo6);
   }
-  if (bola.isTouching(Alvo7) || bola2.isTouching(Alvo7)) {
+  if (bola.isTouching(Alvo7)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo7.destroy();
     bola.bounceOff(Alvo7);
-    bola2.bounceOff(Alvo7);
+   // bola2.bounceOff(Alvo7);
   }
-  if (bola.isTouching(Alvo8) || bola2.isTouching(Alvo8)) {
+  if (bola.isTouching(Alvo8)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo8.destroy();
     bola.bounceOff(Alvo8);
-    bola2.bounceOff(Alvo8);
+   // bola2.bounceOff(Alvo8);
   }
-  if (bola.isTouching(Alvo9) || bola2.isTouching(Alvo9)) {
+  if (bola.isTouching(Alvo9)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo9.destroy();
     bola.bounceOff(Alvo9);
-    bola2.bounceOff(Alvo9);
+   // bola2.bounceOff(Alvo9);
   }
-  if (bola.isTouching(Alvo10) || bola2.isTouching(Alvo10)) {
+  if (bola.isTouching(Alvo10)  /*bola2.isTouching(Alvo2)*/) {
     score = score + 1
     Alvo10.destroy();
     bola.bounceOff(Alvo10);
-    bola2.bounceOff(Alvo10);
+    //bola2.bounceOff(Alvo10);
   }
-
   drawSprites();
 }
 
